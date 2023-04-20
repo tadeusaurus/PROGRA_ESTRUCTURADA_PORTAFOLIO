37f6fa54836131eda8e3d8ef3fffa15f97a1d3f8
tadeo <tadeo.caden0@gmail.com>
2023-04-19 22:38:23 -0600

    006
    operaciones

---
+++ 006 decisiones/19_04_2023_006_Decisiones.cpp
@@ -0,0 +1,30 @@
+// 19_04_2023_006_Decisiones.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+	int character1;
+	int character2;
+	int character3;
+	int character4;
+	int character5;
+	int character6;
+	int character7;
+	int character8;
+	int character9;
+	int character10;
+	int character11;
+	int character12;
+	int character13;
+	int character14;
+	int character15;
+	int character16;
+
+
+}
+
+
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
