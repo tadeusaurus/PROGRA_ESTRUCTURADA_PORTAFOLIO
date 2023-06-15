d26f9eaadc728fe3fbd270fb807c37c1a4f38ef8
tadeo <tadeo.caden0@gmail.com>
2023-06-14 23:59:30 -0600

    Update P17_Matriz_19_05_2023.cpp

--- P17 Matriz dinamica/P17_Matriz_19_05_2023.cpp
+++ P17 Matriz dinamica/P17_Matriz_19_05_2023.cpp
@@ -1,25 +1,40 @@
 // P17_Matriz_19_05_2023.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-#include <Windows.h>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <locale.h>
+
+
+#include <locale.h>    // Para establecer la localización de UTF-8
+#include <Windows.h>   // Para utilizar la función SetConsoleOutputCP
+#include <iostream>    // Para entrada y salida estándar
+#include <cstdlib>     // Para generar números aleatorios con rand
+#include <ctime>       // Para inicializar la semilla de rand
 
 int main() {
-    
-    
+    setlocale(LC_ALL, "es-ES.UTF-8");  // Establecer la localización de UTF-8
+
+    int opcion;
+    std::cout << "Ingrese la opción de una" << std::endl;
+    std::cout << "1. Matriz 3x3" << std::endl;
+    std::cout << "2. Matriz 5x5 " << std::endl;
+    std::cout << "3. Matriz 10x10" << std::endl;
+    std::cout << "Opción: ";
+    std::cin >> opcion;
+
     int filas, columnas;
-   
-    std::cout << "Ingrese el número de filas (mínimo 1): ";
-    std::cin >> filas;
 
-    std::cout << "Ingrese el número de columnas (mínimo 1): ";
-    std::cin >> columnas;
-
-    // Validación del tamaño de la matriz
-    if (filas < 1 || columnas < 1) {
-        std::cout << "El tamaño de la matriz no es válido." << std::endl;
+    switch (opcion) {
+    case 1:
+        filas = 3;
+        columnas = 3;
+        break;
+    case 2:
+        filas = 5;
+        columnas = 5;
+        break;
+    case 3:
+        filas = 10;
+        columnas = 10;
+        break;
+    default:
+        std::cout << "Opción inválida." << std::endl;
         return 0;
     }
 
@@ -30,22 +45,25 @@ int main() {
         matriz[i] = new int[columnas];
     }
 
-    
-    srand(time(NULL)); 
-
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            if (i < 3 && j < 3) {
+    if (opcion == 1) {
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
                 std::cout << "Ingrese el valor para la posición [" << i << "][" << j << "]: ";
                 std::cin >> matriz[i][j];
             }
-            else {
-                matriz[i][j] = rand() % 100; 
+        }
+    }
+    else {
+        srand(time(NULL));
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = rand() % 11; // Relleno aleatorio entre 0 y 10
             }
         }
     }
 
-    std::cout << "Matriz generada:" << std::endl;
+    std::cout << "Matriz:" << std::endl;
 
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < columnas; j++) {
@@ -54,7 +72,6 @@ int main() {
         std::cout << std::endl;
     }
 
-   
     for (int i = 0; i < filas; i++) {
         delete[] matriz[i];
     }
@@ -62,3 +79,6 @@ int main() {
 
     return 0;
 }
+
+
+
