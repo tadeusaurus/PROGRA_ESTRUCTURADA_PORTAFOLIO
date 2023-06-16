676f8273fc29480b25d2d1bad178dd90aa665f91
tadeo <tadeo.caden0@gmail.com>
2023-06-16 16:17:52 -0600

    P20
    TUTORIAL

---
+++ MathClient/MathClient.cpp
@@ -0,0 +1,23 @@
+// MathClient.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// MathClient.cpp
+// compile with: cl /EHsc MathClient.cpp /link MathLibrary.lib
+
+#include <iostream>
+#include "MathLibrary.h"
+
+int main()
+{
+    double a = 7.4;
+    int b = 99;
+
+    std::cout << "a + b = " <<
+        MathLibrary::Arithmetic::Add(a, b) << std::endl;
+    std::cout << "a - b = " <<
+        MathLibrary::Arithmetic::Subtract(a, b) << std::endl;
+    std::cout << "a * b = " <<
+        MathLibrary::Arithmetic::Multiply(a, b) << std::endl;
+    std::cout << "a / b = " <<
+        MathLibrary::Arithmetic::Divide(a, b) << std::endl;
+
+    return 0;
+}
